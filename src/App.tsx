ca9150ad24457ef26e75fefae36ca88457dcc040
Lukasz <lukasz.maniak22@gmail.com>
2025-04-10 19:34:17 +0200

    appSetup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="text-3xl text-red-400 ">Hi</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "var(--text)",
+        background: "var(--background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+      },
+      boxShadow: {
+        hoverState: "5px 5px 0px 0px #111509",
+      },
+    },
+  },
+  plugins: [],
+};
